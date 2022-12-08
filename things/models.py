761941eb1f740f891443e15f89e4123df45ccaf0
SHOZAB ANWAR SIDDIQUE <s.nadeem014@yahoo.com>
2022-12-08 20:39:30 +0000

    Changed model fields to text fields

--- things/models.py
+++ things/models.py
@@ -3,8 +3,8 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here
 class Thing(models.Model):
-    name = models.CharField(max_length=30,unique=True),
-    description = models.CharField(max_length=120,unique=False),
+    name = models.TextField(max_length=30,unique=True),
+    description = models.TextField(max_length=120,unique=False),
     quantity = models.PositiveIntegerField(unique=False,
         validators=[
             MinValueValidator(1), 
